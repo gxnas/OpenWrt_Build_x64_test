73dc62efca2bfbb9f8b226772b669be0ab4e57c9
gxnas <52808122+gxnas@users.noreply.github.com>
2024-09-05 00:04:35 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -4,6 +4,14 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+#添加homeproxy插件
+git clone https://github.com/douglarek/luci-app-homeproxy.git package/luci-app-homeproxy
+
+#echo "添加插件 luci-app-passwall"
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+
+#echo "添加插件 luci-app-ssr-plus"
+git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.1.11"           # 修改openwrt后台地址(填0为关闭)
