6531b7bfaf7905a58a21733629514385075551a6
gxnas <52808122+gxnas@users.noreply.github.com>
2024-09-08 12:56:57 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -10,6 +10,9 @@ git clone https://github.com/douglarek/luci-app-homeproxy.git package/luci-app-h
 #添加luci-app-mosdns插件
 git clone -b v5-lua --depth 1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
+#添加luci-app-daed插件
+git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed package/luci-app-daed
+
 #添加CPU使用率、编译作者、固件下载地址
 sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Compiler author%></td><td><a target="_blank" href="https://wp.gxnas.com">【GXNAS博客】https://wp.gxnas.com</a></td></tr>' package/lean/autocore/files/x86/index.htm
 sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
